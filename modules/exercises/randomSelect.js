c48a805cf74427a449d3ae7d223d68a5bc9c5a66
arissaracode <133154320+Arisforprogramming@users.noreply.github.com>
2024-07-14 22:10:25 -0500

    completed exercises

--- modules/exercises/randomSelect.js
+++ modules/exercises/randomSelect.js
@@ -1,5 +1,14 @@
 function randomFromArray(arr){
   //Your code here to select a random element from the array passed to the function.
+//  let select =  Math.floor(Math.random()*arr.length);
+//     return arr(select);
+let index = Math.floor(Math.random()*arr.length);
+return arr[index];
+
 }
 
+//console.log(randomFromArray(["mama","Water","soysauce"]))
+
 //TODO: Export the randomFromArray function.
+
+module.exports =  randomFromArray;
