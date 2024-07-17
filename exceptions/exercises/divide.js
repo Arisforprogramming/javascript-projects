c620287f2e277439bab595d3201330d16d5fd17a
arissaracode <133154320+Arisforprogramming@users.noreply.github.com>
2024-07-17 00:37:13 -0500

    exercise exeption

--- exceptions/exercises/divide.js
+++ exceptions/exercises/divide.js
@@ -1,7 +1,15 @@
 // Write a function called 'divide' that takes two parameters: a numerator and a denominator.
-
 // Your function should return the result of numerator / denominator.
-
 // However, if the denominator is zero you should throw the error, "Attempted to divide by zero."
-
 // Code your divide function here:
+
+let divide = function (aNumerator, denominator) {
+    let total = 0;
+    total = aNumerator / denominator
+    if (denominator === 0) {
+        throw Error("Attempted to divide by zero.")
+    }
+    return total;
+};
+
+console.log(divide(12, 0));
